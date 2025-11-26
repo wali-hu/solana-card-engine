627d58a37b36d9c31194b538ba133b62f44adf5d
abdullah <abdullah.se4th@gmail.com>
2025-11-26 16:14:01 +0500

    feat:  Mint the tokens to user's token account

--- programs/my-user-card/src/lib.rs
+++ programs/my-user-card/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_lang::system_program;
+use anchor_spl::token;
 
 declare_id!("9PznwD37XbYGLsDPfrxumNBBUY1HeBPb4uneRkX3r8vM");
 
@@ -13,7 +14,6 @@ pub mod user_card_program {
         amount_paid: u64,
         tokens_minted: u64,
     ) -> Result<()> {
-
         // Transfer the amount_paid from authority to user_card account
         let cpi_context = CpiContext::new(
             ctx.accounts.system_program.to_account_info(),
@@ -23,7 +23,35 @@ pub mod user_card_program {
             },
         );
 
+        // SOL transfer
+        msg!(
+            "Transferring {} lamports from authority to user_card account",
+            amount_paid
+        );
         system_program::transfer(cpi_context, amount_paid)?;
+
+        let bump = ctx.bumps.user_card;
+
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"user_card",                        // seed
+            ctx.accounts.authority.key.as_ref(), // user ki public key
+            &[bump],                             // Bump seed
+        ]];
+
+        // Mint the tokens to user's token account
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            token::MintTo {
+                mint: ctx.accounts.card_mint.to_account_info(), // jo token mint karna hai (Gold/Silver etc)
+                to: ctx.accounts.user_token_account.to_account_info(), // User ka wallet jahan token jayega
+                authority: ctx.accounts.user_card.to_account_info(),   // Authority (Hamara PDA)
+            },
+            signer_seeds,
+        );
+
+        // Mint tokens
+        msg!("Minting {} tokens to user's token account", tokens_minted);
+        token::mint_to(cpi_ctx, tokens_minted)?;
 
         let acct = &mut ctx.accounts.user_card;
         acct.owner = ctx.accounts.authority.key();
@@ -54,12 +82,25 @@ pub struct InitializeUserCard<'info> {
     #[account(
         init,
         payer = authority,
-        space = UserCardAccount::SPACE
+        space = UserCardAccount::SPACE,
+        seeds = [b"user_card", authority.key().as_ref()],
+        bump,
     )]
     pub user_card: Account<'info, UserCardAccount>,
 
     #[account(mut)]
-    pub authority: Signer<'info>,
+    pub authority: Signer<'info>, // User
+
+    /// CHECK: Mint account jisse token mint hoga (SPL Mint)
+    #[account(mut)]
+    pub card_mint: Account<'info, token::Mint>,
+
+    /// CHECK: User's token account jahan token jayega (SPL Token Account)
+    #[account(mut)]
+    pub user_token_account: Account<'info, token::TokenAccount>,
+
+    /// Required SPL token program
+    pub token_program: Program<'info, token::Token>,
 
     pub system_program: Program<'info, System>,
 }
